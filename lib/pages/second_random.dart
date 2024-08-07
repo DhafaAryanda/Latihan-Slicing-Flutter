55ad01c31ebb18c8e129cb17aaff22c5c3a6acb3
Dhafa <dhofaaryanda4@gmail.com>
2024-08-07 15:01:48 +0700

    update: finish

--- lib/pages/second_random.dart
+++ lib/pages/second_random.dart
@@ -35,117 +35,134 @@ class SecondRandom extends StatelessWidget {
             height: 37,
           ),
           Padding(
-              padding: const EdgeInsets.only(
-                left: 24.0,
-              ),
-              child: Column(
+            padding: const EdgeInsets.only(
+              left: 24.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'About',
+                  style: GoogleFonts.poppins(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  'Pantai Pandawa adalah salah satu para \nkawasan wisata di area Kuta selatan sana \nKabupaten Dekat Bandung, Bali.',
+                  style: GoogleFonts.poppins(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300,
+                    color: Color(0xff2F323A),
+                  ),
+                ),
+                SizedBox(
+                  height: 26,
+                ),
+                Text(
+                  'Booking Now',
+                  style: GoogleFonts.poppins(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Image.asset(
+                      'assets/date_one.png',
+                      width: 80,
+                      height: 100,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Image.asset(
+                      'assets/date_two.png',
+                      width: 80,
+                      height: 100,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Image.asset(
+                      'assets/date_three.png',
+                      width: 80,
+                      height: 100,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Image.asset(
+                      'assets/date_four.png',
+                      width: 80,
+                      height: 100,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(
+          left: 20,
+          right: 20,
+        ),
+        child: BottomNavigationBar(
+          elevation: 0,
+          type: BottomNavigationBarType.fixed,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'About',
+                    '\$22,800',
                     style: GoogleFonts.poppins(
-                      fontSize: 16,
+                      fontSize: 22,
                       fontWeight: FontWeight.w500,
-                      color: Colors.black,
+                      color: Color(0xff3F6DF6),
                     ),
                   ),
                   Text(
-                    'Pantai Pandawa adalah salah satu para \nkawasan wisata di area Kuta selatan sana \nKabupaten Dekat Bandung, Bali.',
+                    '/Night',
                     style: GoogleFonts.poppins(
-                      fontSize: 16,
+                      fontSize: 12,
                       fontWeight: FontWeight.w300,
                       color: Color(0xff2F323A),
                     ),
-                  ),
-                  SizedBox(
-                    height: 26,
-                  ),
-                  Text(
-                    'Booking Now',
-                    style: GoogleFonts.poppins(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.black,
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Image.asset(
-                        'assets/date_one.png',
-                        width: 80,
-                        height: 100,
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Image.asset(
-                        'assets/date_two.png',
-                        width: 80,
-                        height: 100,
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Image.asset(
-                        'assets/date_three.png',
-                        width: 80,
-                        height: 100,
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Image.asset(
-                        'assets/date_four.png',
-                        width: 80,
-                        height: 100,
-                      ),
-                    ],
                   )
                 ],
               ),
-              bottomNavigationBar: BottomNavigationBar(
-                type: BottomNavigationBarType.fixed,
-                items: <BottomNavigationBarItem>[
-                  BottomNavigationBarItem(
-                    icon: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          '\$22,800',
-                          style: GoogleFonts.poppins(
-                            fontSize: 22,
-                            fontWeight: FontWeight.w500,
-                            color: Color(0xff3F6DF6),
-                          ),
-                        ),
-                        Text(
-                          '/Night',
-                          style: GoogleFonts.poppins(
-                            fontSize: 12,
-                            fontWeight: FontWeight.w300,
-                            color: Color(0xff2F323A),
-                          ),
-                        )
-                      ],
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Container(
+                width: 220,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Color(0xff3F6DF6),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Continue',
+                    style: GoogleFonts.poppins(
+                      color: Color(0xffFAFAFA),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
                     ),
-                    label: '',
                   ),
-                  BottomNavigationBarItem(
-                    icon: Column(
-                      children: [
-                        Text(
-                          '\$22,800',
-                        ),
-                        Text(
-                          '/Night',
-                        )
-                      ],
-                    ),
-                    label: '',
-                  ),
-                ],
-              ))
-        ],
+                ),
+              ),
+              label: '',
+            ),
+          ],
+        ),
       ),
     );
   }
